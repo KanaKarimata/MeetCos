c5d1b7ec4df13c201f395278ce48a8e772b59a81
KanaKarimata <kana.lt22@gmail.com>
2022-06-14 08:02:20 +0000

    [修正中]通知機能

--- app/controllers/public/notifications_controller.rb
+++ app/controllers/public/notifications_controller.rb
@@ -1,6 +1,6 @@
 class Public::NotificationsController < ApplicationController
   def index
-    @notifications = current_user.receive_notifications.page(params[:page].per(20))
+    @notifications = current_user.receive_notifications.page(params[:page])
     @notifications.where(checked: false).each do |notification|
       @notification.update_attributes(checked: true)
     end
