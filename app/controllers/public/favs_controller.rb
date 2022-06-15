ff9ab05dd7c52d0a7f2c44de22e02b5544087d0b
KanaKarimata <kana.lt22@gmail.com>
2022-06-15 03:05:58 +0000

    [修正中]いいねの非同期化

--- app/controllers/public/favs_controller.rb
+++ app/controllers/public/favs_controller.rb
@@ -1,16 +1,16 @@
 class Public::FavsController < ApplicationController
   def create
     post = Post.find(params[:post_id])
-    fav = current_user.favs.new(post_id: post.id)
-    fav.save
+    @fav = current_user.favs.new(post_id: post.id)
+    @fav.save
     post.create_notification_fav!(current_user)
-    redirect_to post_path(post)
+    render 'replace_btn'
   end
 
   def destroy
     post = Post.find(params[:post_id])
-    fav = current_user.favs.find_by(post_id: post.id)
-    fav.destroy
-    redirect_to post_path(post)
+    @fav = current_user.favs.find_by(post_id: post.id)
+    @fav.destroy
+    render 'replace_btn'
   end
 end
