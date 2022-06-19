1053667de9808af7ef9b07f1542d2f65b54a63f8
KanaKarimata <kana.lt22@gmail.com>
2022-06-19 15:38:13 +0000

    [追加]管理者側ユーザー一覧ページ

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,16 @@
+class Admin::UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.update(is_deleted: "退会") #ここでis_deletedカラムの値を"退会"に更新
+     reset_session
+     redirect_to root_path
+  end
+end
