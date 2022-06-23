195cda64b7df62953769a69c51d2270d18c12e4e
KanaKarimata <kana.lt22@gmail.com>
2022-06-23 13:02:36 +0000

    [edit]

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,7 +12,6 @@ import "popper.js";
 import "bootstrap";
 import "../stylesheets/application.scss"
 import '@fortawesome/fontawesome-free/js/all'
-import '../stylesheets/mystyle.css.scss'
 // import '../stylesheets/slick-theme.scss'
 // import '../stylesheets/slick.scss'
 
